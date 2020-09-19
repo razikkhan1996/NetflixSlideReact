3a071a3542c621c7d9fa0930b0aac19fb062a8f9
Razik <razik99815@gmail.com>
2020-09-19 20:31:18 +0530

    Updates

---
+++ precache-manifest.b1e2b9827aae5d6c19b08f46b314bca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f6ce155ef9afb30f0547f9b3310bcf",
+    "url": "/NetflixSlideReact/index.html"
+  },
+  {
+    "revision": "d2ee8ac6fc7591cdbe3d",
+    "url": "/NetflixSlideReact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e37119311c721f19f259",
+    "url": "/NetflixSlideReact/static/css/main.dffefbe2.chunk.css"
+  },
+  {
+    "revision": "d2ee8ac6fc7591cdbe3d",
+    "url": "/NetflixSlideReact/static/js/2.5ffdf1ea.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/NetflixSlideReact/static/js/2.5ffdf1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37119311c721f19f259",
+    "url": "/NetflixSlideReact/static/js/main.89028374.chunk.js"
+  },
+  {
+    "revision": "eabba7cf7ec9c292644d",
+    "url": "/NetflixSlideReact/static/js/runtime-main.262a2459.js"
+  }
+]);
